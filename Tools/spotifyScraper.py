addaefb79f44fc2792c367b441b546c9391097f0
Philip Gray <philiprgray@msn.com>
2022-03-30 18:05:20 -0400

    added our very first tool :)

---
+++ Tools/spotifyScraper.py
@@ -0,0 +1,17 @@
+import configparser
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pprint
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+auth_manager = SpotifyClientCredentials(client_id=config['spotify']['id'], client_secret=config['spotify']['secret'])
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+
+q = 'name:Environment&type=show,episode&market=US'
+result = sp.search(q="Red Tide", type="show,episode", market="US")
+
+pprint.pprint(result)
+search = "red tide"
