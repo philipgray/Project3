14a33dd6f68a2195499608423aedbd108aab560c
AlexWills37 <alexanderwills37@gmail.com>
2022-04-20 21:55:37 -0400

    Added placeholder-y things

--- florida-dashboard/src/app/app.module.ts
+++ florida-dashboard/src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { GoogleChartsModule } from 'angular-google-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestBoxComponent } from './test-box/test-box.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { NavigationButtonComponent } from './navigation-button/navigation-button.component';
+import { YoutubeVideoComponent } from './youtube-video/youtube-video.component';
+import { SafePipe } from './pipes/safe.pipe';
+import { YoutubePanelComponent } from './youtube-panel/youtube-panel.component';
+import { BarGraphComponent } from './bar-graph/bar-graph.component';
+import { TweetListComponent } from './tweet-list/tweet-list.component';
+import { TweetComponent } from './tweet/tweet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestBoxComponent,
     NavigationBarComponent,
-    NavigationButtonComponent
+    NavigationButtonComponent,
+    YoutubeVideoComponent,
+    SafePipe,
+    YoutubePanelComponent,
+    BarGraphComponent,
+    TweetListComponent,
+    TweetComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    GoogleChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ florida-dashboard/src/app/bar-graph/bar-graph.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'angular-google-charts';
+
+// For information on how to change properties of the chart,
+// see https://github.com/FERNman/angular-google-charts
+// (in particular look at the Readme file)
+
+@Component({
+  selector: 'app-bar-graph',
+  template: `
+
+    <!-- Uses google chart wrapper -->
+    <google-chart #chart
+      title="Tweets over time"
+      [type]="this.chartType"
+      [data]="this.tweetFrequency"
+
+      ></google-chart>
+  `,
+  styleUrls: ['./bar-graph.component.css']
+})
+export class BarGraphComponent implements OnInit {
+
+  chartType = ChartType.ColumnChart;
+
+  tweetFrequency = [
+    ["June 2017", 30],
+    ["July 2017", 40],
+    ["August 2017", 49],
+    ["September 2017", 17],
+    ["October 2017", 11]
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
